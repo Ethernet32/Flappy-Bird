b45b0ec91504d5b7ca39a33de2ca3722b7755666
ethernet32 <ethan.ralphs@ucas-edu.net>
2024-11-08 11:49:51 -0700

    made bird flap and rotate

--- game.py
+++ game.py
@@ -8,6 +8,12 @@ class Game:
         self.pipe = pygame.image.load(pipe_img).convert_alpha()
         self.background = pygame.image.load(background_img).convert_alpha()
         self.ground = pygame.image.load(ground_img).convert_alpha()
+        self.ground_position = 0
+        self.active = True
+        self.gravity = 0.05
+        self.bird_movement = 0
+        self.rotated_bird = pygame.Surface((0,0))
+
     def resize_images(self):
         self.bird = pygame.transform.scale(self.bird, (51,34))
         self.pipe = pygame.transform.scale(self.pipe, (80,439))
@@ -16,3 +22,28 @@ class Game:
 
     def show_background(self, screen):
         screen.blit(self.background, (0,0))
+
+    def show_ground(self, screen):
+        screen.blit(self.ground, (self.ground_position, 650))
+
+    def move_ground(self):
+        self.ground_position -= 1
+        self.ground_position %= 45
+        self.ground_position -=45
+    
+    def show_bird(self, screen):
+        screen.blit(self.rotated_bird, self.bird_rect)
+
+    def update_bird(self):
+        self.bird_movement += self.gravity
+        self.rotated_bird = self.rotate_bird()
+        self.bird_rect.centery += self.bird_movement
+
+    def rotate_bird(self):
+        #rotozoom takes into acount the surfaface rotation and zoom
+        new_bird = pygame.transform.rotozoom(self.bird, -self.bird_movement *3, 1)
+        return new_bird
+    
+    def flap(self):
+        self.bird_movement = 0
+        self.bird_movement -= 2.5
