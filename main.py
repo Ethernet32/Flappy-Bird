5f7732bf0cdd890cd845bbf6970893df02499627
Ethernet32 <ethan.ralphs@ucas-edu.net>
2024-12-13 23:55:52 -0700

    Update main.py
    enemy can kill player

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import pygame
 import sys
-from game import Game
+from game import *
+from fractions import Fraction
 
-pygame.init()
-screen = pygame.display.set_mode((400, 720))
-clock = pygame.time.Clock() 
-
-SPAWNPIPE = pygame.USEREVENT
-pygame.time.set_timer(SPAWNPIPE, 1800)
-
-game = Game("img/bird.png", "img/pipe.png", "img/background.png", "img/ground.png")
+screen = pygame.display.set_mode((850, 750))
+game = Game()
 game.resize_images()
+clock = pygame.time.Clock()
+
+SPAWNENEMY = pygame.USEREVENT + 1
+pygame.time.set_timer(SPAWNENEMY, 1000)
 
 while True:
     for event in pygame.event.get():
@@ -18,32 +17,47 @@ while True:
             pygame.quit()
             sys.exit()
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE and game.active:
-                game.flap()
-            if event.key == pygame.K_SPACE and game.active == False:
-                game.restart( )
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            game.add_bullet(*pygame.mouse.get_pos())
 
-        if event.type == SPAWNPIPE:
-            game.add_pipe()
+        if pygame.mouse.get_focused():
+            game.point_at(*pygame.mouse.get_pos())
 
+        if event.type == SPAWNENEMY:
+            game.add_enemy(850/2, 750/2)
+        
 
-    game.show_background(screen)
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_d]:
+        if pressed[pygame.K_w] or pressed[pygame.K_s] or pressed[pygame.K_a]:
+            game.move_right(3)
+        else:
+            game.move_right(4)
+    if pressed[pygame.K_a]:
+        if pressed[pygame.K_w] or pressed[pygame.K_s] or pressed[pygame.K_d]:
+            game.move_left(3)
+        else:
+            game.move_left(4)
+    if pressed[pygame.K_w]:
+        if pressed[pygame.K_d] or pressed[pygame.K_s] or pressed[pygame.K_a]:
+            game.move_up(3)
+        else:
+            game.move_up(4)
+    if pressed[pygame.K_s]:
+        if pressed[pygame.K_d] or pressed[pygame.K_w] or pressed[pygame.K_a]:
+            game.move_down(3)
+        else:
+            game.move_down(4)
     
-    game.show_ground(screen)
-    game.move_ground()
-
-    if game.active:
-        game.show_bird(screen)
-        game.update_bird()
-        game.move_pipe()
-        game.show_pipes(screen)
-        game.check_collision()
-        game.update_score()
-        game.show_score("playing", screen, (255,255,255))
-    else:
-        game.game_over(screen, (255,255,255))
-
+    if game.active == True:
+        game.show_floor(screen)
+        game.show_bullets(screen)
+        game.move_bullet()
+        game.move_enemy()
+        game.show_enemy(850/2, 750/2)
+        game.show_player(screen)
+        game.p_e_collision()
+        game.b_e_collision()
 
     pygame.display.update()
     clock.tick(120)
