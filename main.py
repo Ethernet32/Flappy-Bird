5ecd0bd57e063f4015df1cb2a1426989b66d15e7
Ethernet32 <ethan.ralphs@ucas-edu.net>
2024-12-16 11:20:33 -0700

    Update main.py
    fixed

--- main.py
+++ main.py
@@ -1,22 +1,49 @@
 import pygame
 import sys
 from game import Game
+
 pygame.init()
-screen = pygame.display.set_mode((720,720))
-clock = pygame.time.Clock()
-#We need the pictures here:
-game = Game('img/character.png')
+screen = pygame.display.set_mode((400, 720))
+clock = pygame.time.Clock() 
+
+SPAWNPIPE = pygame.USEREVENT
+pygame.time.set_timer(SPAWNPIPE, 1800)
+
+game = Game("img/bird.png", "img/pipe.png", "img/background.png", "img/ground.png")
 game.resize_images()
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE and game.active:
-                game.shoot()        
+                game.flap()
             if event.key == pygame.K_SPACE and game.active == False:
-                game.restart()
+                game.restart( )
+
+        if event.type == SPAWNPIPE:
+            game.add_pipe()
+
+
+    game.show_background(screen)
+    
+    game.show_ground(screen)
+    game.move_ground()
+
     if game.active:
-        game.show_character(screen)
-        game.turn_character() 
+        game.show_bird(screen)
+        game.update_bird()
+        game.move_pipe()
+        game.show_pipes(screen)
+        game.check_collision()
+        game.update_score()
+        game.show_score("playing", screen, (255,255,255))
+    else:
+        game.game_over(screen, (255,255,255))
+
+
+    pygame.display.update()
+    clock.tick(120)
